2695b1b0e3713e07a55aa7810d3bc6488f7964e0
吃嘛嘛香香 <291419752@qq.com>
2021-12-28 17:49:28 +0800

    x

--- dzl1.js
+++ dzl1.js
@@ -230,7 +230,9 @@ async function getck() {
         $[_0xd0c2('0x2ca', 'FQ^r')](JSON['stringify'](dzlapp, null, 0x2), _0xd0c2('0xa5', '#C9R'));
         $[_0xd0c2('0xcf', 'g&)K')]($['name'], '多走路账号' + (_0x2e4440 + 0x1) + _0xd0c2('0x277', 'sdNI'));
         console.log('输出ck')
-        console.log(dzlapp)
+        console.log(_0x521077)
+        console.log(_0x2866e4)
+        console.log(_0x5c71c2)
     }
     if ($request[_0xd0c2('0xbf', '!C4G')]['match'](/\/withdraw\/apply/)) {
         const _0x2fccd1 = $request['body'];
