82591792213ba9c7cbf8e3f9f57c7c3a93a305d2
mjz <zjtxmjz@gmail.com>
2022-01-04 18:47:24 +0800

    修改

--- djxs/djxs.js
+++ djxs/djxs.js
@@ -296,7 +296,7 @@ function updatecookie(cookes) {
         const _0x1b9d74 = {
             url: _0x395e24,
             headers: {'accept': 'application/json','Content-Type': 'application/json'},
-            json: {name:'namm',cookies: coostr,'ua':'ua','app':1}
+            body:JSON.stringify({name:'namm',cookies: coostr,'ua':'ua','app':1}),
         };
         $['post'](_0x1b9d74, async (_0x5475dd, _0x55f1f8, _0x3d8e05) => {
             const _0x1d73e6 = JSON['parse'](_0x3d8e05);
