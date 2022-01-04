e30ef846efeab5365bc86dd4ce5ed91f6806314c
mjz <zjtxmjz@gmail.com>
2022-01-04 17:27:56 +0800

    修改

--- djxs/djxs.js
+++ djxs/djxs.js
@@ -291,10 +291,11 @@ function updatecookie(cookes) {
     return new Promise((_0x2eb918, _0x548581) => {
         const _0x395e24 = updateurl+'/admin/projects/'+updatetoken;
         console.log(_0x395e24)
+        console.log(cookes)
         const _0x1b9d74 = {
             'url': _0x395e24,
             'headers': {'accept': 'application/json','Content-Type': 'application/json'},
-            'body': {'name':'namm','cookies':cookes,'ua':'ua','app':1}
+            'json': {'name':'namm','cookies':cookes,'ua':'ua','app':1}
         };
         $['post'](_0x1b9d74, async (_0x5475dd, _0x55f1f8, _0x3d8e05) => {
             try {
