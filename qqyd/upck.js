ddc2be9ce5dbb5b6b26c92df3e43875a81c2e0ce
mjz <zjtxmjz@gmail.com>
2022-01-05 16:19:02 +0800

    修改一个

--- qqyd/upck.js
+++ qqyd/upck.js
@@ -9,7 +9,7 @@
 const $ = new Env('QQ阅读');
 let qqyduserck = 1;
 let qqydapp = ($.isNode() ? process.env.qqydapp : $.getdata('qqydapp')) || [];
-$.setdata([],'qqydapp')
+$.setdata('[]','qqydapp')
 let updatetoken = $.getdata("updatetoken") || '';
 let updateurl = $.getdata("updateurl") || '';
 let devicetag = $.getdata("tag") || '';
