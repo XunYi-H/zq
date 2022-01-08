e1510899dc09d49d9aacc5453692fa9c8279fc13
mjz <zjtxmjz@gmail.com>
2022-01-08 16:14:14 +0800

    修改

--- awy/upck.js
+++ awy/upck.js
@@ -63,7 +63,7 @@ function upck(ua,cookie) {
         const body = {
             url: url,
             headers: {'accept': 'application/json','Content-Type': 'application/json'},
-            body:datas,
+            body:JSON.stringify(datas),
         };
         $.post(body, (err, resp, data) => {
             try {
