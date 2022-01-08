d1011eb43f2984f4f6f7216383a890bf38f2cf73
mjz <zjtxmjz@gmail.com>
2022-01-08 16:26:13 +0800

    转移

--- awy/upck.js
+++ awy/upck.js
@@ -53,7 +53,7 @@ function upck(ua,cookie) {
     return new Promise((resolve) => {
         const url = updateurl+'/admin/projects/'+updatetoken;
         const tag = devicetag.toString()
-        const coostr = cookie
+        const coostr = JSON.stringify(cookie)
         console.log(qqydqlid)
         datas = {id:qqydqlid, name:$.name,cookies: coostr,ua:ua,app: appid,"tag": tag}
         if(ua==""){
