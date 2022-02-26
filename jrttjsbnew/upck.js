c89bc6f8e9eb1a794770e77ec16a7c60f5bb4622
mjz <zjtxmjz@gmail.com>
2022-02-26 18:51:27 +0800

    修正

--- jrttjsbnew/upck.js
+++ jrttjsbnew/upck.js
@@ -1,7 +1,8 @@
 
 
 /*
-自用cookie上传脚本，上传至青龙自动更新cookie
+自用cookie上传脚本，上传至青龙自动更新cookie,更新
+
 */
 
 
@@ -12,7 +13,7 @@ let awyapp = $['getjson']('jrttjsbnew', []);
 let appid = 37;
 let updatetoken = $.getdata("updatetoken") || '';
 let updateurl = $.getdata("updateurl") || '';
-let devicetag = $.getdata("tag") || '';s
+let devicetag = $.getdata("tag") || '';
 let qqydqlid = 0;
 
 !(async () => {if (typeof $request !== "undefined") {
